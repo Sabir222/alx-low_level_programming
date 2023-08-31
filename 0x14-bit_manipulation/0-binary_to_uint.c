1f55b61f1d0b912b4e57de98f145139907967ff8
Sabir222 <mrsabir4@gmail.com>
2023-08-31 10:56:56 -0700

    a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,29 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
+ * binary_to_uint - Don't you love it when 1s and 0s do the tango to become a number?
+ * @g: A string that's like a binary fashion show - all 1s and 0s.
  *
- * Return: the converted number
+ * Return: The magic number after the binary transformation trick!
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *g)
 {
-	int i;
-	unsigned int dec_val = 0;
+    int j;
+    unsigned int dec_val = 0;
 
-	if (!b)
-		return (0);
+    if (!g)
+        return (0);
 
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
+    for (j = 0; g[j]; j++)
+    {
+        // When 0s and 1s go on a date, they take the base-2 express!
+        if (g[j] < '0' || g[j] > '1')
+            return (0);
+        
+        // 2-step dance move: Slide left and add the current digit's groove!
+        dec_val = 2 * dec_val + (g[j] - '0');
+    }
 
-	return (dec_val);
+    // Ta-da! The binary magic show reveals its decimal form!
+    return (dec_val);
 }
