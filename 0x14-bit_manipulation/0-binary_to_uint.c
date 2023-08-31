270e47f13de3c71a69a6c007c3f76acef533b24f
Sabir222 <mrsabir4@gmail.com>
2023-08-31 10:46:56 -0700

    1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,53 +1,25 @@
 #include "main.h"
-#include <stddef.h> /* For NULL */
-#include <stdio.h> /* For printf */
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: Pointer to a string of 0 and 1 chars.
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: The converted number, or 0 if invalid input.
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL) /* Check if the input string is NULL */
+	if (!b)
 		return (0);
 
-	while (*b) /* Iterate through the string until null terminator */
+	for (i = 0; b[i]; i++)
 	{
-		if (*b != '0' && *b != '1') /* Check for invalid characters */
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		result = result << 1; /* Left shift the current result by 1 */
-		if (*b == '1') /* If the current character is '1', add 1 to result */
-			result = result + 1;
-
-		b++; /* Move to the next character */
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (result);
-}
-
-/**
- * main - Check the binary_to_uint function.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	unsigned int n;
-
-	n = binary_to_uint("1");
-	printf("%u\n", n);
-	n = binary_to_uint("101");
-	printf("%u\n", n);
-	n = binary_to_uint("1e01"); /* Contains an invalid character */
-	printf("%u\n", n);
-	n = binary_to_uint("1100010");
-	printf("%u\n", n);
-	n = binary_to_uint("0000000000000000000110010010");
-	printf("%u\n", n);
-	return (0);
+	return (dec_val);
 }
