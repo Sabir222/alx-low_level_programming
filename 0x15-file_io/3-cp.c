5159e01f615b298a8db8389410e9194e24f8459c
Sabir222 <mrsabir4@gmail.com>
2023-09-04 15:48:34 -0700

    a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,104 +1,105 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h" /* Include the header file "main.h". */
+#include <stdio.h> /* Include the standard I/O library. */
+#include <stdlib.h> /* Include the standard library for memory allocation and other functions. */
+
+char *create_buffer(char *file); /* Function prototype for create_buffer. */
+void close_file(int fd); /* Function prototype for close_file. */
 
 /**
- * create_buffer - Hires a buffer for data storage, 1024 bytes at a time.
- * @file: The name of the lucky file that gets to have its data buffered.
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
  *
- * Return: A pointer to the freshly hired buffer.
+ * Return: A pointer to the newly-allocated buffer.
  */
 char *create_buffer(char *file)
 {
-    /* We're about to hire a buffer, hope it doesn't ask for a raise! */
-    char *buffer;
+	char *buffer; /* Declare a character pointer named buffer. */
 
-    buffer = malloc(sizeof(char) * 1024);
+	buffer = malloc(sizeof(char) * 1024); /* Allocate memory for the buffer. */
 
-    if (buffer == NULL)
-    {
-        dprintf(STDERR_FILENO,
-                "Oops! Something went wrong. We ran out of buffers for file %s\n", file);
-        exit(99); // Our buffer factory went on strike!
-    }
+	if (buffer == NULL) /* Check if the allocation was successful. */
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99); /* Exit the program with error code 99. */
+	}
 
-    return (buffer); // The new buffer is ready to work!
+	return (buffer); /* Return a pointer to the allocated buffer. */
 }
 
 /**
- * close_file - Gives the file descriptor its walking papers.
- * @fd: The file descriptor to be dismissed.
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
  */
 void close_file(int fd)
 {
-    /* Time to say goodbye to our clingy file descriptor! */
-    int c;
+	int c; /* Declare an integer variable named c. */
 
-    c = close(fd);
+	c = close(fd); /* Close the file descriptor. */
 
-    if (c == -1)
-    {
-        dprintf(STDERR_FILENO, "Oops! We can't close fd %d. It's too clingy!\n", fd);
-        exit(100); // File descriptor refused to leave the party!
-    }
+	if (c == -1) /* Check if the closing operation failed. */
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't close fd %d\n", fd);
+		exit(100); /* Exit the program with error code 100. */
+	}
 }
 
 /**
- * main - The great file copying spectacle begins!
- * @argc: The number of arguments our performers have.
- * @argv: An array of pointers to the arguments, the stars of the show.
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: 0 if the act was successful.
+ * Return: 0 on success.
  *
- * Description: If the argument count is incorrect, we'll exit with code 97, as in "oops, wrong stage!"
- * If file_from is a no-show or camera-shy, it's exit code 98 for them.
- * If file_to couldn't find a dressing room or had a writer's block, it's exit code 99.
- * If file_to or file_from can't take a hint and leave after the show, it's exit code 100.
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
-    /* Let the file copying circus begin! */
-    int from, to, r, w;
-    char *buffer;
+	int from, to, r, w; /* Declare integer variables. */
+	char *buffer; /* Declare a character pointer named buffer. */
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97); // Someone forgot their lines, exit stage left!
-    }
+	if (argc != 3) /* Check if the number of command-line arguments is not 3. */
+	{
+		dprintf(STDERR_FILENO,
+			"Usage: cp file_from file_to\n");
+		exit(97); /* Exit the program with error code 97. */
+	}
 
-    buffer = create_buffer(argv[2]);
-    from = open(argv[1], O_RDONLY);
-    r = read(from, buffer, 1024);
-    to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = create_buffer(argv[2]); /* Call the create_buffer function to allocate a buffer. */
+	from = open(argv[1], O_RDONLY); /* Open the source file for reading. */
+	r = read(from, buffer, 1024); /* Read data from the source file into the buffer. */
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664); /* Open the destination file for writing. */
 
-    do {
-        /* The performers are ready to amaze the audience! */
-        if (from == -1 || r == -1)
-        {
-            dprintf(STDERR_FILENO,
-                    "Uh-oh, file %s didn't show up for rehearsal!\n", argv[1]);
-            free(buffer);
-            exit(98); // The show must go on, but not without the main act!
-        }
+	do { /* Start a loop to copy data from source to destination. */
+		if (from == -1 || r == -1) /* Check for read errors. */
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer); /* Free the allocated buffer. */
+			exit(98); /* Exit the program with error code 98. */
+		}
 
-        w = write(to, buffer, r);
-        if (to == -1 || w == -1)
-        {
-            dprintf(STDERR_FILENO,
-                    "Oops! File %s refused to accept the script!\n", argv[2]);
-            free(buffer);
-            exit(99); /*File got stage fright, couldn't write a single line!*/
-        }
+		w = write(to, buffer, r); /* Write data from the buffer to the destination file. */
+		if (to == -1 || w == -1) /* Check for write errors. */
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer); /* Free the allocated buffer. */
+			exit(99); /* Exit the program with error code 99. */
+		}
 
-        r = read(from, buffer, 1024);
-        to = open(argv[2], O_WRONLY | O_APPEND);
+		r = read(from, buffer, 1024); /* Read more data from the source file into the buffer. */
+		to = open(argv[2], O_WRONLY | O_APPEND); /* Open the destination file for appending. */
 
-    } while (r > 0);
+	} while (r > 0); /* Continue the loop until there's no more data to read. */
 
-    free(buffer);
-    close_file(from);
-    close_file(to);
+	free(buffer); /* Free the allocated buffer. */
+	close_file(from); /* Close the source file. */
+	close_file(to); /* Close the destination file. */
 
-    return (0); /*Standing ovation! The file-copying extravaganza was a success!*/
+	return (0); /* Return 0 to indicate successful execution. */
 }
